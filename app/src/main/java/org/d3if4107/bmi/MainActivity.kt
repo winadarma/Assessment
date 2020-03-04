065791c2944e760a76e045839a32d0536ce41ba9
Praktikan <winadarma234@gmail.com>
2020-03-04 10:31:55 +0700

    initial commit

---
+++ app/src/main/java/org/d3if4107/bmi/MainActivity.kt
@@ -0,0 +1,21 @@
+package org.d3if4107.bmi
+
+import androidx.databinding.DataBindingUtil
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
